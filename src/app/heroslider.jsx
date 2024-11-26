3ed5566904afa3cb25c685cc4071e03417af28d2
anuxr4g <anun20003@gmail.com>
2024-11-26 20:18:14 +0530

    Initial Commit

--- src/app/heroslider.jsx
+++ src/app/heroslider.jsx
@@ -4,11 +4,11 @@ import { Typewriter } from 'react-simple-typewriter';
 
 const Heroslider = () => {
   return (
-    <div className="p-8">
-      <div className="flex flex-col sm:flex-row items-center justify-center bg-white text-nowrap mb-2">
-        <h1 className="text-black font-black text-center text-2xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-8xl 2xl:text-9xl leading-none tracking-tight">
+    <div className="relative p-8 h-screen">
+      <div className="absolute bottom-4 left-4 z-10 text-left flex">
+        <h1 className="text-white font-black text-2xl sm:text-4xl md:text-5xl lg:text-[88px] xl:text-8xl 2xl:text-[150px] leading-none tracking-tight">
           NEBULA{' '}
-          <span className="text-black">
+          <span className="text-white">
             <Typewriter
               words={['VENTURE', 'INCUBATOR', 'ACCELERATOR']}
               loop={0}
@@ -16,21 +16,19 @@ const Heroslider = () => {
               cursorStyle='_'
               typeSpeed={100}
               deleteSpeed={100}
-              delaySpeed={2000}
+              delaySpeed={1000}
             />
           </span>
         </h1>
-        <p className="text-black text-sm sm:text-lg md:text-2xl lg:text-3xl xl:text-4xl font-semibold mt-2 sm:mt-0 sm:ml-4">
-          &nbsp;EST.<br /> 2021
+        <p className="text-white text-sm sm:text-lg md:text-2xl lg:text-3xl xl:text-4xl font-semibold mt-2">
+          EST.<br /> 2021
         </p>
       </div>
-      <div className="relative w-full h-[calc(100vh-12rem)] overflow-hidden">
-        <div className="relative">
-          <video className="w-full h-[90vh] object-cover object-center" loop autoPlay muted>
-            <source src="https://res.cloudinary.com/dxwea32ih/video/upload/v1731327818/qb301sp1orhuhu1fzoyl.mp4" type="video/mp4" />
-          </video>
-          <div className="lg:absolute lg:top-0 lg:left-0 lg:w-full lg:h-screen xl:h-full lg:bg-black lg:opacity-30"></div>
-        </div>
+      <div className="absolute inset-0 overflow-hidden">
+        <video className="w-full h-full object-cover" loop autoPlay muted>
+          <source src="https://res.cloudinary.com/dxwea32ih/video/upload/v1731327818/qb301sp1orhuhu1fzoyl.mp4" type="video/mp4" />
+        </video>
+        <div className="absolute inset-0 bg-black opacity-30"></div>
       </div>
     </div>
   );
